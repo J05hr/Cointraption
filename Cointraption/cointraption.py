cf118131ecb19ca3b7b08d3780953b28c5d1a193
J05h <J05h@protonmail.com>
2020-12-20 17:35:44 -0500

    setup basic framework

--- Cointraption/cointraption.py
+++ Cointraption/cointraption.py
@@ -7,8 +7,8 @@ import sys
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    stylePath = str(Path.cwd()) + '\\layouts\\stylesheets\\light.qss'
-    utils.toggle_stylesheet(stylePath)
+    # stylePath = str(Path.cwd()) + '\\layouts\\stylesheets\\light.qss'
+    # utils.toggle_stylesheet(stylePath)
     win = main_window.MainWindow()
     win.show()
     sys.exit(app.exec())
